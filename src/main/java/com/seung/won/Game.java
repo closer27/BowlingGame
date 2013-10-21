b6c2e7614e60cd8540b3bcb2ec11d19a623a10a1
Seungwon Kim <closer527@gmail.com>
2013-10-21 20:04:43 +0900

    modification that getCurrentScore is able to calculate Spare

--- src/main/java/com/seung/won/Game.java
+++ src/main/java/com/seung/won/Game.java
@@ -11,6 +11,7 @@ public class Game {
 
     int[] droppedPins;
     private int currentRoll;
+    private boolean isSpare = false;
 
     public Game() {
         droppedPins = new int[22];
@@ -19,12 +20,25 @@ public class Game {
 
     public void roll(int pin) {
         droppedPins[currentRoll] = pin;
+        currentRoll++;
     }
 
     public int getCurrentScore() {
         int score = 0;
+//        for (int i=0; i<currentRoll; i++) {
+//            score += droppedPins[i];
+//        }
+
         for (int i=0; i<currentRoll; i++) {
-            score += droppedPins[i];
+            if(i%2 == 0) { // 한 레일의 첫번째 롤
+                score += droppedPins[i];
+            }
+            else {  // 두번째 롤
+                score += droppedPins[i];
+                if (droppedPins[i-1]+droppedPins[i] == 10) {
+                    score += droppedPins[i+1];
+                }
+            }
         }
         return score;
     }
--- src/test/java/com/seung/won/GameTest.java
+++ src/test/java/com/seung/won/GameTest.java
@@ -12,12 +12,23 @@ import org.junit.Test;
  */
 public class GameTest {
     @Test
-    public void testRoll() {
+    public void testZeroRoll() {
         Game game = new Game();
         game.roll(0);
         game.roll(0);
         game.roll(0);
 
         Assert.assertEquals(game.getCurrentScore(), 0);
+    }
+
+    @Test
+    public void testNumberRoll() {
+        Game game = new Game();
+        game.roll(2);
+        game.roll(8);
+        game.roll(5);
+        game.roll(3);
+
+        Assert.assertEquals(game.getCurrentScore(), 23);
     }
 }
